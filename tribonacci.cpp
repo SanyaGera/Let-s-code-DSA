3047a2cb7035622426a94478e9acc5c23a610a41
Sachin Kumar Sharma <89345567+sksachin7z2@users.noreply.github.com>
2022-10-08 13:09:32 +0530

    Update tribonacci.cpp

--- tribonacci.cpp
+++ tribonacci.cpp
@@ -1,4 +1,13 @@
-   #include<bits/stdc++.h>
+/* The tribonacci series is a generalization of the Fibonacci sequence where each term is the sum of the three preceding terms.
+
+ a(n) = a(n-1) + a(n-2) + a(n-3) 
+ with 
+ a(0) = a(1) = 0, a(2) = 1.    
+
+ Input : 5
+ Output : 0, 0, 1, 1, 2  */
+
+#include<bits/stdc++.h>
    using namespace std;
 
    int fib(int n,vector<int>&dp){
